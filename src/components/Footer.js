34896c5f7f9458b7563d7f9fa70779443a354b50
Ken Hauter <kenneth.hauter@gmail.com>
2025-02-21 18:48:11 +0900

    Add component file with header, nav, main, and footer files
    added in a component with the four main components
    and placeholder text

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import '../App.css';
+function Footer() {
+    return (
+        <footer className='App'>
+            This is the footer section of the website
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import '../App.css';
+
+function Main() {
+    return (
+        <main className='App'>
+            This is the main part!
+        </main>
+    )
+}
+export default Main;
