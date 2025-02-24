7d83496df513cb0d9d75afd69f41dba30b7158d0
Ken Hauter <kenneth.hauter@gmail.com>
2025-02-24 15:57:14 +0900

    Add basic html setup for layout of the website
    added in link placeholders for the website on the
    nav and footer sections.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,14 @@ import '../App.css';
 function Nav() {
 return (
     <nav className='App'>
-        This is the nav section for the website links!
+        <ul className='flex flex-col gap-2'>
+            <li><a href='url'>Home</a></li>
+            <li><a href='url'>About</a></li>
+            <li><a href='url'>Menu</a></li>
+            <li><a href='url'>Reservations</a></li>
+            <li><a href='url'>Order Online</a></li>
+            <li><a href='url'>Login</a></li>
+        </ul>
     </nav>
 )
 }
