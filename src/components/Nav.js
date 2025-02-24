f483ec04b48d123ca7924080331ab56dce4d1ea2
Ken Hauter <kenneth.hauter@gmail.com>
2025-02-24 17:13:20 +0900

    Add styling to remove link styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import '../App.css';
 function Nav() {
 return (
     <nav className='App'>
-        <ul className='flex flex-col gap-2'>
+        <ul className='links'>
             <li><a href='url'>Home</a></li>
             <li><a href='url'>About</a></li>
             <li><a href='url'>Menu</a></li>
