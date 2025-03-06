0946fba313613f7fe09e7605db17ae7c64c5ae91
Ken Hauter <kenneth.hauter@gmail.com>
2025-03-06 20:18:57 +0900

    Add Navbar styling and smaller screen Navbar
    added an offcanvas navbar for smaller screen sizes

---
+++ src/components/Menu.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { Col }from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  Image from 'react-bootstrap/Image';
+import OffcanvasHeader from 'react-bootstrap/OffcanvasHeader';
+import '../App.css';
+
+const Menu = () => {
+  const [show, setShow] = useState(false);
+  const handleShow =()=>{setShow(true);}
+  const handleClose =()=> {setShow(false);};
+  return (
+   <>
+   <Button
+   onClick={handleShow} 
+   className='d-lg-none position-absolute top-10 end-0 translate-middle btn-color'>
+    <Image src='/Images/menu-1.jpg' fluid/>
+   </Button>
+    <Offcanvas 
+    show={show} 
+    onHide={handleClose} 
+    placement='end'
+    responsive='lg'>
+       <Container fluid>
+      <OffcanvasHeader closeButton>
+        <Col>
+        <Offcanvas.Title className='position-absolute top-0 start-10 mt-5'>
+          Navigation
+        </Offcanvas.Title>
+        </Col>
+        <Col>
+        <Offcanvas.Body className='mt-3'>
+            <Nav className='justify-content-end flex-grow-1 pe-3'>
+              <Nav.Link className='text-dark' href='#'>Home</Nav.Link>
+              <Nav.Link className='text-dark' href='#'>About</Nav.Link>
+              <Nav.Link className='text-dark' href='#'>Menu</Nav.Link>
+              <Nav.Link className='text-dark' href='#'>Reservations</Nav.Link>
+              <Nav.Link className='text-dark' href='#'>Order Online</Nav.Link>
+              <Nav.Link className='text-dark' href='#'>Login</Nav.Link>
+            </Nav>
+        </Offcanvas.Body>
+        </Col>
+      </OffcanvasHeader>
+      </Container>
+    </Offcanvas>
+   </>
+  )
+}
+
+export default Menu;
