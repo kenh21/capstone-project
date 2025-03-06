a9a9b282ee41f64604f5a3fb3852b3617f43591c
Ken Hauter <kenneth.hauter@gmail.com>
2025-03-06 20:18:57 +0900

    Add Navbar styling and smaller screen Navbar
    added an offcanvas navbar for smaller screen sizes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Header from './components/Header';
-import Nav from './components/Nav'
+import Nav from './components/Nav';
+import Menu from './components/Menu'
 import Main from './components/Main';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
     <div className='header'>
       <Header />
       <Nav />
+        
     </div>
     <div className='main'>
     <Main />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,17 @@
 import '../App.css';
+import Menu from './Menu';
 function Nav() {
 return (
     <nav className='nav'>
         <ul className='links'>
-            <li><a href='url'>Home</a></li>
-            <li><a href='url'>About</a></li>
-            <li><a href='url'>Menu</a></li>
-            <li><a href='url'>Reservations</a></li>
-            <li><a href='url'>Order Online</a></li>
-            <li><a href='url'>Login</a></li>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Menu</a></li>
+            <li><a href='#'>Reservations</a></li>
+            <li><a href='#'>Order Online</a></li>
+            <li><a href='#'>Login</a></li>
         </ul>
+        <Menu />
     </nav>
 )
 }
