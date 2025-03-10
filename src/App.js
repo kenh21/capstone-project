22a548f84dc78231e6cdd218f16ee80485a5770f
Ken Hauter <kenneth.hauter@gmail.com>
2025-03-10 17:45:55 +0900

    Add bootstrap grids to website layout
    updated the website layout to use bootstrap grids
    layout

--- src/App.js
+++ src/App.js
@@ -8,34 +8,40 @@ import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Testimonials from './components/Testimonials';
 import About from './components/About';
+import Container  from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function App() {
   return (
     < >
-    <div className='layout'>
-    <div className='header'>
-      <Header />
-      <NavBar />
-      <Menu />
-    </div>
-    <div className='main'>
-      <div className='hero'>
-      <Hero />
-    </div>
-    <Main />
-    <Highlights />
-    </div>
-    <div className='testimonials'>
-      <Testimonials />
-    </div>
-      <div className='about'>
-        <About />
-      </div>
-    <div className='footer'>
-    <Footer />
-    </div>
-    </div>
+    <Container fluid className='m-0 p-0'>
+      <Row className='header'>
+        <Col>
+          <Header />
+        </Col>
+        <Col>
+          <NavBar />
+          <Menu />
+        </Col>
+        </Row>
+        <Row className='hero'>
+          <Hero />
+        </Row>
+        <Row>
+          <Highlights />
+        </Row>
+        <Row className='testimonials'>
+          <Testimonials />
+          </Row>
+        <Row>
+          <About />
+        </Row>
+        <Row className='footer'>
+          <Footer />
+        </Row>
+    </Container>
     </>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,27 @@
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Image from "react-bootstrap/image";
+import '../App.css';
+
 
 function Hero() {
     return (
-    <Container>
-    <p>Hero section!</p>
+    <Container fluid>
+    <Row>
+        <Col>
+        <h1 className="hero-section-h1-text">Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>Velit sint ea incididunt velit commodo amet aute et in sunt deserunt ipsum sint minim.</p>
+        <Button>
+            Reserve a Table
+        </Button>
+        </Col>
+        <Col className=" d-flex justify-content-center align-items-center">
+            <Image src='/Images/hero-image.jpg' alt='hero-image' className="
+            m-5
+            rounded-4
+            " />
+        </Col>
+    </Row>
     </Container>
 );
 };
