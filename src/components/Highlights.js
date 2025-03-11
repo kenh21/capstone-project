b5dfeb1d2142a17f873391c12fd3f74db46f7234
Ken Hauter <kenneth.hauter@gmail.com>
2025-03-11 16:11:31 +0900

    Add styling and spacing to Highlights Section
    added styling and spacing to header section of the
    Highlights Section

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,7 +1,35 @@
+import { Col, Row, Container, Button } from "react-bootstrap";
+import '../App.css';
 
 function Highlights () {
     return (
-        <p>Highlights Section!</p>
+        <Container fluid>
+            <Row>
+                <Col className=" 
+                d-flex
+                justify-content-start
+                align-items-center
+                mt-5
+                ms-5">
+                <h2>This Weeks Specials!</h2>
+                </Col>
+                <Col className=" 
+                d-flex 
+                justify-content-end 
+                me-5 
+                mt-5 
+                pe-5">
+                    <Button href='#' type="submit" className="
+                    p-3 
+                    main-button">
+                    Online Menu
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+
+            </Row>
+        </Container>
 );
 };
 
