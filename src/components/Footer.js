0871a4482a7020b3ec213439c874e41b1a1b57bd
Ken Hauter <kenneth.hauter@gmail.com>
2025-03-21 16:45:50 +0900

    Add image and links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
+import { Col, Row, Container, Image, Nav } from 'react-bootstrap';
+import FooterImg from '../Images/footer.jpeg';
+
 function Footer() {
     return (
-        <footer className='footer'>
-            <img src='url'/>
-           <nav>
-        <ul>
-            <li><a href='url'>Home</a></li>
-            <li><a href='url'>About</a></li>
-            <li><a href='url'>Menu</a></li>
-            <li><a href='url'>Reservations</a></li>
-            <li><a href='url'>Order Online</a></li>
-            <li><a href='url'>Login</a></li>
-        </ul>
-    </nav>
-    <ul>
-            <li><a href='url'>Address</a></li>
-            <li><a href='url'>Phone Number</a></li>
-            <li><a href='url'>Email</a></li>
-        </ul>
-        <ul>
-            <li><a href='url'>Address</a></li>
-            <li><a href='url'>Phone Number</a></li>
-            <li><a href='url'>Email</a></li>
-        </ul>
-        </footer>
+        <Container fluid className='footer p-5'>
+            <Row className=''>
+            <Col>
+            <Image src={FooterImg} width={195} className='img-border-style ms-5' alt='Owners standing in front of a stove top'/>
+            </Col>
+            <Col>
+           <Nav className='d-flex flex-column justify-content-space-around ms-3'>
+              <Nav.Link className='text-dark fw-bolder ' href='#'>Home</Nav.Link>
+              <Nav.Link className='text-dark fw-bolder ' href='#'>About</Nav.Link>
+              <Nav.Link className='text-dark fw-bolder ' href='#'>Menu</Nav.Link>
+              <Nav.Link className='text-dark fw-bolder ' href='#'>Reservations</Nav.Link>
+              <Nav.Link className='text-dark fw-bolder ' href='#'>Order Online</Nav.Link>
+              <Nav.Link className='text-dark fw-bolder ' href='#'>Login</Nav.Link>
+        </Nav>
+        </Col>
+        <Col>
+            <Nav className='d-flex flex-column justify-content-space-around'>
+                <Nav.Link className='text-dark fw-bolder ' href='#'>Address</Nav.Link>
+                <Nav.Link className='text-dark fw-bolder ' href='#'>Phone Number</Nav.Link>
+                <Nav.Link className='text-dark fw-bolder ' href='#'>Email</Nav.Link>
+            </Nav>
+        </Col>
+        <Col>
+            <Nav className='d-flex flex-column justify-content-space-around'>
+                <Nav.Link className='text-dark fw-bolder ' href='#'>Address</Nav.Link>
+                <Nav.Link className='text-dark fw-bolder ' href='#'>Phone Number</Nav.Link>
+                <Nav.Link className='text-dark fw-bolder ' href='#'>Email</Nav.Link>
+            </Nav>
+        </Col>
+        </Row>
+        </Container>
     )
 }
 
