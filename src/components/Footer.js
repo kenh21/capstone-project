8c37b9fd59bb2e81e38f683e126a244f1097c00e
Ken Hauter <kenneth.hauter@gmail.com>
2025-03-24 17:23:36 +0900

    Add OnlineMenu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,24 +10,24 @@ function Footer() {
             <Col>
             <Image src={FooterImg} width={195} className='img-border-style ms-5' alt='Owners standing in front of a stove top'/>
             </Col>
-            <Col>
+            <Col className='mt-2'>
            <Nav className='d-flex flex-column justify-content-space-around ms-3'>
               <Nav.Link className='text-dark fw-bolder ' href='#'>Home</Nav.Link>
               <Nav.Link className='text-dark fw-bolder ' href='#'>About</Nav.Link>
               <Nav.Link className='text-dark fw-bolder ' href='#'>Menu</Nav.Link>
               <Nav.Link className='text-dark fw-bolder ' href='#'>Reservations</Nav.Link>
-              <Nav.Link className='text-dark fw-bolder ' href='#'>Order Online</Nav.Link>
+              <Nav.Link style={{height: 40}} className='text-dark fw-bolder ' href='#'>Order Online</Nav.Link>
               <Nav.Link className='text-dark fw-bolder ' href='#'>Login</Nav.Link>
         </Nav>
         </Col>
-        <Col>
-            <Nav className='d-flex flex-column justify-content-space-around ms-lg-3 ms-md-3 ms-sm-5 ms-xs-3'>
+        <Col className='mt-2'>
+            <Nav className='d-flex flex-column justify-content-space-around ms-lg-3 ms-md-3 ms-sm-3 ms-xs-3'>
                 <Nav.Link className='text-dark fw-bolder ' href='#'>Address</Nav.Link>
                 <Nav.Link className='text-dark fw-bolder ' href='#'>Phone Number</Nav.Link>
                 <Nav.Link className='text-dark fw-bolder ' href='#'>Email</Nav.Link>
             </Nav>
         </Col>
-        <Col>
+        <Col className='mt-2'>
             <Nav className='d-flex flex-column justify-content-space-around ms-3'>
                 <Nav.Link className='text-dark fw-bolder ' href='#'>Address</Nav.Link>
                 <Nav.Link className='text-dark fw-bolder ' href='#'>Phone Number</Nav.Link>
---
+++ src/components/OnlineMenu.js
@@ -0,0 +1,8 @@
+
+function OnlineMenu () {
+    return (
+        <h1>Menu</h1>
+    )
+}
+
+export default OnlineMenu;
