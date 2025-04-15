8c0769fcaa4647929c99c2bd58dcd29fe89268e3
Ken Hauter <kenneth.hauter@gmail.com>
2025-04-15 17:40:45 +0900

    Add reservation form template

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,7 +1,34 @@
+import { Container, Row } from "react-bootstrap";
 
 function Reservations () {
   return (
-    <div>Reservations</div>
+  <>
+  <Container fluid>
+  <form>
+    <Row>
+    <label for="res-date">Choose Date</label>
+    <input type="date" id="res-date"></input>
+    <label for="res-time">Choose Time</label>
+    <select id="res-time">
+      <option>17:00</option>
+      <option>18:00</option>
+      <option>19:00</option>
+      <option>20:00</option>
+      <option>21:00</option>
+      <option>22:00</option>
+    </select>
+    <label for="guests">Number of Guests</label>
+    <input type="number" placeholder="1" min={1} max={10} id="guests"></input>
+    <label for="occasion">Occasion</label>
+    <select id="occasion">
+      <option>Birthday</option>
+      <option>Anniversary</option>
+    </select>
+    <input type="submit" value="Make Your reservation"></input>
+    </Row>
+  </form>
+  </Container>
+  </>
   )
 }
 
