6a006ed8d7cdcec60354d5c082e3e15e76224365
Ken Hauter <kenneth.hauter@gmail.com>
2025-04-16 17:05:53 +0900

    Add styling to Reservations page

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,11 +1,28 @@
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
+import NavBar from "../components/NavBar"
+import  Header from "../components/Header"
+import Restaurant from "../Images/restaurant.jpg"
+import '../App.css';
 
 function Reservations () {
   return (
   <>
-  <Container fluid>
+  <Container fluid className="m-0 p-0 vh-100"> 
+    <Row className="me-5">
+      <Col className="ms-5 py-3">
+      <Header />
+      </Col>
+      <Col className="py-3 align-content-center">
+      <NavBar />
+      </Col>
+    </Row>
+    <Row className="justify-content-center pb-5">
+    <Image src={Restaurant} className="rest-img"/>
+    </Row>
+    <Container fluid className="hero vh-100">
   <form>
-    <Row>
+    <Row className="justify-content-evenly">
+      <Col className="col-1">
     <label for="res-date">Choose Date</label>
     <input type="date" id="res-date"></input>
     <label for="res-time">Choose Time</label>
@@ -25,8 +42,10 @@ function Reservations () {
       <option>Anniversary</option>
     </select>
     <input type="submit" value="Make Your reservation"></input>
+    </Col>
     </Row>
   </form>
+  </Container>
   </Container>
   </>
   )
